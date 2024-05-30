ecff37e712869fce013c814442e4009b9a07c855
Vib-UX <vibhav@instaraise.io>
2024-05-31 00:23:12 +0530

    nit: emoji sort

--- src/components/ui/marketPlaceCard.tsx
+++ src/components/ui/marketPlaceCard.tsx
@@ -13,7 +13,6 @@ const MarketPlaceCard = ({
   modelId: string;
   ipfsUrl: string;
 }) => {
-
   const [isHovered, setIsHovered] = useState(false);
   const [teaseData, setTeaseData] = useState({
     name: '',
@@ -141,9 +140,7 @@ export const MarketPlaceCard2 = ({
   modelName: string;
   image: string;
 }) => {
-
   const [buttonText, setButtonText] = useState('Accept the call 📞');
-
 
   // useEffect(() => {
   //   const fetchTeaseData = async (ipfs: string) => {
@@ -207,21 +204,31 @@ export const MarketPlaceCard2 = ({
         <div className='absolute top-1 left-1/2 -translate-x-1/2 mx-auto w-full p-1 text-white block'>
           <div className='flex items-center justify-center w-full pt-3 gap-3 pb-2'>
             <p className='capitalize text-xl font-bold flex gap-1 justify-center items-center'>
-              {modelName} is calling... ❤️
-
+              💋 {modelName} is calling... ❤️
             </p>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-8 ringing">
-              <path fillRule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clipRule="evenodd" />
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 24 24'
+              fill='currentColor'
+              className='size-8 ringing'
+            >
+              <path
+                fillRule='evenodd'
+                d='M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z'
+                clipRule='evenodd'
+              />
             </svg>
           </div>
         </div>
-        <motion.div
-          className=' w-full z-50 p-1  h-full flex flex-col items-center justify-end'
-        >
+        <motion.div className=' w-full z-50 p-1  h-full flex flex-col items-center justify-end'>
           <div className='px-3 w-full flex items-center justify-center pb-2'>
             <button
-              className={`w-[80%] mx-auto h-[45px] z-20 relative rounded-xl ${buttonText === 'Accept the call 📞' ? 'bg-green-500 hover:bg-green-600' : 'bg-[#fb0393] hover:bg-opacity-80'} bg-opacity-50`}
-              onMouseEnter={() => setButtonText('Try your luck 🌟')} // Change text on hover
+              className={`w-[80%] mx-auto h-[45px] z-20 relative rounded-xl ${
+                buttonText === 'Accept the call 📞'
+                  ? 'bg-green-500 hover:bg-green-600'
+                  : 'bg-[#fb0393] hover:bg-opacity-80'
+              } bg-opacity-45`}
+              onMouseEnter={() => setButtonText('🎲 Try your luck 🌟')} // Change text on hover
               onMouseLeave={() => setButtonText('Accept the call 📞')} // Revert text on mouse leave
             >
               <motion.span
